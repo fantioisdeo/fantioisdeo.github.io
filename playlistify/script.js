41fd5e958b9f528bfe5678def1ea907d573458fe
tasyanadia <tasyanadia2410@gmail.com>
2024-10-09 15:53:38 +0700

    fitur link

--- playlistify/script.js
+++ playlistify/script.js
@@ -1,6 +1,7 @@
 function addSong() {
     const songTitle = document.getElementById("songTitle").value;
     const artistName = document.getElementById("artistName").value;
+    const linkSong = document.getElementById("linkSong").value;
 
     if (songTitle === "" || artistName === "") {
         alert("Mohon masukkan judul lagu dan nama artis.");
@@ -9,13 +10,26 @@ function addSong() {
 
     const playlist = document.getElementById("playlist");
     const songItem = document.createElement("li");
-    songItem.innerHTML = `${songTitle} - ${artistName} <button onclick="removeSong(this)">Hapus</button>`;
+    songItem.innerHTML = `
+            <div>
+                <div>
+                    ${songTitle} - ${artistName} 
+                </div>
+            </div>
+            <div style="display:flex; gap:4px;">
+                <button>
+                    <a href="${linkSong}" target="_blank">Link</a>
+                </button>
+                <button onclick="removeSong(this)">Hapus</button>
+            </div>
+    `;
     
     playlist.appendChild(songItem);
 
     // Reset input fields
     document.getElementById("songTitle").value = "";
     document.getElementById("artistName").value = "";
+    document.getElementById("linkSong").value = "";
 }
 
 function removeSong(button) {
