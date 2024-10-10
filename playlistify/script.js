273ade75a1cc8af3a90ae8525ad9495534477512
yotamputra <yotamppy@gmail.com>
2024-10-10 12:56:50 +0700

    final??

--- playlistify/script.js
+++ playlistify/script.js
@@ -6,13 +6,13 @@ if (name) {
     document.getElementById('greet').innerText = "Guest";
 }
 
-
 function addSong() {
     const songTitle = document.getElementById("songTitle").value;
     const artistName = document.getElementById("artistName").value;
+    const linkSong = document.getElementById("linkSong").value;
 
     if (songTitle === "" || artistName === "") {
-        alert("Please enter the song title and artist name.");
+        alert("Please input Song Title and Artist Name.");
         return;
     }
 
@@ -20,13 +20,23 @@ function addSong() {
     playlistTable.row.add([
         songTitle,
         artistName,
-        `<button class="update-button" onclick="updateSong(this)">Edit</button>
-         <button class="remove-button" onclick="removeSong(this)">Hapus</button>`
+        `
+        <div class="aksi">
+            <a class="link-button" href="${linkSong}" target="_blank">
+                <button class="link-button">
+                    Lets Go!
+                </button>
+            </a>
+            <button class="update-button" onclick="updateSong(this)">Edit</button>
+            <button class="remove-button" onclick="removeSong(this)">Delete</button>
+        </div>
+        `
     ]).draw();
 
     // Reset input fields
     document.getElementById("songTitle").value = "";
     document.getElementById("artistName").value = "";
+    document.getElementById("linkSong").value = "";
 }
 
 function removeSong(button) {
@@ -39,15 +49,23 @@ function updateSong(button) {
     const row = $(button).closest('tr');
     const data = $('#playlist').DataTable().row(row).data();
 
-    const newSongTitle = prompt("Edit judul lagu:", data[0]);
-    const newArtistName = prompt("Edit nama artis:", data[1]);
+    const newSongTitle = prompt("Edit Song Title:", data[0]);
+    const newArtistName = prompt("Edit Artist Name:", data[1]);
+    const newLink = prompt("Edit link:", data[2].match(/href="([^"]+)"/)[1]);
 
     if (newSongTitle !== null && newArtistName !== null && newSongTitle !== "" && newArtistName !== "") {
         $('#playlist').DataTable().row(row).data([
             newSongTitle,
             newArtistName,
-            `<button class="update-button" onclick="updateSong(this)">Edit</button>
-             <button class="remove-button" onclick="removeSong(this)">Hapus</button>`
+            `<div class="aksi">
+                <a class="link-button" href="${newLink}" target="_blank">
+                    <button class="link-button">
+                        Lets Go!
+                    </button>
+                </a>
+                <button class="update-button" onclick="updateSong(this)">Edit</button>
+                <button class="remove-button" onclick="removeSong(this)">Delete</button>
+            </div>`
         ]).draw();
     }
 }
